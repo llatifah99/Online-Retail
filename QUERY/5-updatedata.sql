f8879e7ab6d55c14ecb0085af47e9fc510daec0e
llatifah99 <litalatifah6@gmail.com>
2024-01-15 10:36:23 +0700

    5. update data

---
+++ QUERY/5-updatedata.sql
@@ -0,0 +1,35 @@
+UPDATE kategori
+SET nama = 'Electronic'
+WHERE nama = 'Elektronik';
+
+UPDATE pembeli
+SET alamat = 'Jl. Ahmad Yani no 11A Surabaya'
+WHERE nama = 'Lita';
+
+UPDATE pembeli
+SET alamat = 'Jl. Adisucipto No. 15 Jogja-Solo'
+WHERE nama = 'Jihan';
+
+UPDATE pembeli
+SET alamat = 'Jl. Diponegoro no 5 Surabaya'
+WHERE nama = 'Nafisah';
+
+UPDATE pembeli
+SET alamat = 'Jl. Gatot Subroto Jakarta'
+WHERE nama = 'Anya';
+
+UPDATE produk
+SET harga = 110000
+WHERE nama = 'Magic com mini';
+
+UPDATE produk
+SET harga = 60000
+WHERE nama = 'Kemeja';
+
+UPDATE produk
+SET harga = '48000'
+WHERE nama = 'Lampu Tidur';
+
+UPDATE produk
+SET harga = 100000
+WHERE nama = 'Blouse';
